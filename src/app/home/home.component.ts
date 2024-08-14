0d303aec8d78bf498c6a7b294e62ea0cd841b71e
Pat <patb.software@gmail.com>
2024-08-14 17:43:54 +0700

    Add filtering function for search bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,34 +10,38 @@ import { HousingService } from '../housing.service';
   imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
-      <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
-      </form>
+      <div>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
+      </div>
     </section>
 
     <section class="results">
-      <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
+      <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css']
 })
 
-// @for (housingLocation of housingLocationList; track housingLocation.id) {
-//   <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
-// }
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
     this.housingService.getAllHousingLocations().then(
       (housingLocationList: HousingLocation[]) => {
         this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
       }
     );
+  }
 
-    console.log(this.housingLocationList);
-    
+  filterResults(text: string) {
+    if (!text) this.filteredLocationList = this.housingLocationList;
+    else this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+
   }
 }
